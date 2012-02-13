364d53952555f7e186a4f68939128786fb0d1a8e
Dmitry Erman <dmitry@kilbasa.com>
2012-02-13 11:32:55 -0800

    sass-lang.gem v3.1.12

---
+++ bin/sass.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"jruby-complete-1.6.5.1.jar" "C:/dev/gem-jars/sass-gems/bin/sass" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"jruby-complete-1.6.5.1.jar" "%~dpn0" %*
